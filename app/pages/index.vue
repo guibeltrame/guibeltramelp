acf6c71311f9ed1e23076cb45e7640b419b58d98
Guilherme Beltrame <guilhermebeltrame@Mac-de-Giu.local>
2026-08-27 14:22:40 -0300

    paginaaberta

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,9 +19,7 @@ useHead({
 <template>
   <div>
     <LandingVideoIntro />
- 
-    <main id="esconder" style="display: none;">
-    <LandingPainIdentification />
+   <LandingPainIdentification />
       <LandingProblemReframe />
       <LandingAuthoritySection />
       <LandingMethodPillars />
@@ -38,6 +36,8 @@ useHead({
       <LandingTransformationSection />
       <LandingFAQSection />
       <LandingFinalCTASection /> 
+    <main id="esconder" style="display: none;">
+  
      
     </main>
     
